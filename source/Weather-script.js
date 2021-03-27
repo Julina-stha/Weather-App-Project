9b01004f5ed5a2a0e4a3ec6f9dbde006ce58fc95
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-03-27 21:09:04 +0000

    added more to the showFahrenheit function

--- source/Weather-script.js
+++ source/Weather-script.js
@@ -1,25 +1,39 @@
 ///Setting the Time and Date///
-
 let current = new Date();
 let currentDate = current.getDate();
 if (currentDate < 10) {
   currentDate = `0${currentDate}`;
 }
-let months = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"];
+let months = [
+  "01",
+  "02",
+  "03",
+  "04",
+  "05",
+  "06",
+  "07",
+  "08",
+  "09",
+  "10",
+  "11",
+  "12",
+];
 let currentMonth = months[current.getMonth()];
 let currentYear = current.getFullYear();
 let displayDate = document.querySelector("#date");
 displayDate.innerHTML = `${currentDate}/${currentMonth}/${currentYear}`;
-
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 let currentDay = days[current.getDay()];
 let displayDay = document.querySelector("#day");
 displayDay.innerHTML = currentDay;
-
-
-
-
-
 function formatHours(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -32,16 +46,12 @@ function formatHours(timestamp) {
   }
   return `${hours}:${minutes}`;
 }
-
-
-function formatWeekday(timestamp) {
+function formatWeekDay(timestamp) {
   let date = new Date(timestamp);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
-  let dailyWeekDays = days[current.getDay()];
-  
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+  let dailyWeekDays = days[date.getDay()];
   return `${dailyWeekDays}`;
 }
-
 function formatLocalTime(timeZone, dateTime) {
   let now = new Date();
   if (dateTime !== null) {
@@ -52,8 +62,6 @@ function formatLocalTime(timeZone, dateTime) {
   let convertedTime = now.getTime() + timeZoneOffsetInMs + timeZoneInMs;
   return convertedTime;
 }
-
-
 function formatIcon(icon) {
   let iconElement = null;
   if (icon === "01d") {
@@ -95,7 +103,6 @@ function formatIcon(icon) {
   }
   return iconElement;
 }
-
 function displayWeatherInfo(outcome) {
   console.log(outcome);
   document.querySelector("#city-name").innerHTML = outcome.data.name;
@@ -121,52 +128,47 @@ function displayHourlyForecast(response) {
   console.log(response.data);
   hourlyForecastElement.innerHTML = null;
   let hourlyForecast = null;
-  
   for (let index = 0; index < 4; index++) {
     let hourlyForecast = response.data.list[index];
-    let localTimestamp = hourlyForecast.dt + response.data.city.timezone
+    let localTimestamp = hourlyForecast.dt + response.data.city.timezone;
     hourlyForecastElement.innerHTML += `
     <div class="list-group-item-2 list-group-item-action">
       ${formatHours(localTimestamp * 1000)}
       <span class="second-section-icon">
       <img src="${formatIcon(hourlyForecast.weather[0].icon)}" width="48"/>
         <span class="third-section-degree">
-          ${Math.round(hourlyForecast.main.temp)}°
-        </span>
+          ${Math.round(hourlyForecast.main.temp)}
+        </span>°
       </span>
-    </div>`
+    </div>`;
   }
 }
 //display hourly forecast end//
-
 //Daily forecast start//
 function getDailyForecast(cityLatitude, cityLongitude) {
   let apiKey = "ba753d969dccd2973e89444d00d45191";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${cityLatitude}&lon=${cityLongitude}&exclude=current,minutely,hourly,alerts&units=metric&appid=${apiKey}`;
   axios.get(apiUrl).then(displayDailyForecast);
-
 }
-
 function displayDailyForecast(response) {
   let dailyForecastElement = document.querySelector("#daily-forecast");
   console.log(response.data);
   dailyForecastElement.innerHTML = null;
   let dailyForecast = null;
-
-  for (let index = 1; index < 4; index++) {
-    let dailyForecast = response.data.list[index];
+  for (let index = 0; index < 4; index++) {
+    let dailyForecast = response.data.daily[index];
     dailyForecastElement.innerHTML += `
     <div class="list-group-item list-group-item-action">
-      "${formatWeekDay (dailyForecast.dt * 1000)}"
+      "${formatWeekDay(dailyForecast.dt * 1000)}"
       <span class="second-section-icon">
       <img src="${formatIcon(dailyForecast.weather[0].icon)}" width="60" />
-        <span class="second-section-degree">
-          ${Math.round(dailyForecast.temp.day)}°
-        </span>
+      <div class="second-section-degree">
+        ${Math.round(dailyForecast.temp.day)}°
+      </div>°
       </span>
-    </div>`
+    </div>`;
   }
-} 
+}
 //Daily forecast end//
 
 //search box start//
@@ -174,25 +176,19 @@ function searchCity(results) {
   let apiKey = "ba753d969dccd2973e89444d00d45191";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${results}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherInfo);
-
   let apiHourlyForecastUrlstring = `https://api.openweathermap.org/data/2.5/forecast?q=${results}&appid=${apiKey}&units=metric`;
   axios.get(apiHourlyForecastUrlstring).then(displayHourlyForecast);
- 
 }
 searchCity("Kathmandu");
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-bar").value;
   searchCity(city);
 }
-
 let searchForm = document.querySelector("#search-bar");
 searchForm.addEventListener("submit", handleSubmit);
-
 let searchButton = document.querySelector("#on-click");
 searchButton.addEventListener("click", handleSubmit);
-
 //search box end//
 
 //Locate button start//
@@ -206,15 +202,12 @@ function showLocation(position) {
   let apiHourlyForecastUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiHourlyForecastUrl).then(displayHourlyForecast);
 }
-  
 function getLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(showLocation);
 }
-
 let locateButton = document.querySelector("#locate");
 locateButton.addEventListener("click", getLocation);
-
 // Locate button end//
 
 //show fahrenheit start//
@@ -223,20 +216,44 @@ function showFahrenheit(event) {
   let fahrenheitFormula = (celsiusTemp * 9) / 5 + 32;
   if (event.target.checked) {
     switchF.innerHTML = `${Math.round(fahrenheitFormula)}°F`;
+    let dailyForecast = document.querySelectorAll(".second-section-degree");
+    dailyForecast.forEach(function (item) {
+      // grabbing the current value to convert
+      let currentTemp = item.innerHTML;
+      // convert to Fahrenheit
+      item.innerHTML = Math.round((currentTemp * 9) / 5 + 32);
+    });
+    let hourlyForecast = document.querySelectorAll(".third-section-degree");
+    hourlyForecast.forEach(function (item) {
+      // grabbing the current value to convert
+      let currentTemp = item.innerHTML;
+      // convert to Fahrenheit
+      item.innerHTML = Math.round((currentTemp * 9) / 5 + 32);
+    });
   } else {
     switchF.innerHTML = `${Math.round(celsiusTemp)}°C`;
+    let dailyForecast = document.querySelectorAll(".second-section-degree");
+    dailyForecast.forEach(function (item) {
+      // grabbing the current value to convert
+      let currentTemp = item.innerHTML;
+      // convert to Celsius
+      item.innerHTML = Math.round(((currentTemp - 32) * 5) / 9);
+    });
+    let hourlyForecast = document.querySelectorAll(".third-section-degree");
+    hourlyForecast.forEach(function (item) {
+      // grabbing the current value to convert
+      let currentTemp = item.innerHTML;
+      // convert to Celsius
+      item.innerHTML = Math.round(((currentTemp - 32) * 5) / 9);
+    });
   }
 }
-
 function hideCelsius() {
   let celsius = document.querySelector("#unit");
   celsius.innerHTML = "";
 }
-
 let celsiusTemp = null;
-
 let switchButton = document.querySelector("#flexSwitchCheckDefault");
 switchButton.addEventListener("click", showFahrenheit);
 switchButton.addEventListener("click", hideCelsius);
 //show fahrenheit end//
-
