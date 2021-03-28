a12356831f67ffebbede6452f5fe95457c006363
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-03-28 21:04:48 +0100

    added credits - html

--- source/Weather-script.js
+++ source/Weather-script.js
@@ -270,7 +270,7 @@ switchButton.addEventListener("click", hideCelsius);
 //show fahrenheit end//
 
 //set background start//
-let formatMonth = currentMonth;
+let formatMonth = 12;
 if (formatMonth >= 3 && formatMonth <= 5) {
   document.body.style.backgroundImage = "url(source/images/Spring.png)";
   document.body.style.backgroundRepeat = "no-repeat";
